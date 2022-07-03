62b37bec4ad4c27ce8d4fd662fe478a41cff7541
Angelo-Bruno <brunoangelo.dev@gmail.com>
2022-07-03 17:04:23 -0100

    minor change

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const cors = require('cors')
 
 
-const PORT = process.event.PORT || 3333;
+const PORT = process.env.PORT || 3333;
 const app = express();
 
 app.use(cors({
